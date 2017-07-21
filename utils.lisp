71bf2d0f80f084b6ce3d7ec69374fea32a25c070
Anish Moorthy <anishmoorthy@idevicesinc.com>
2017-07-21 13:11:12 -0400

    Refactor prime checkers to step by 6

--- utils.lisp
+++ utils.lisp
@@ -10,13 +10,23 @@
   )
 
 (defun primes-leq (upbound)
+
   (if (< upbound 2)
       (return-from primes-leq nil))
-  (let* ((prime-list (list 2)) (i 3))
-    (loop while (<= i upbound) do
-      (when (not (divisor-in-list i prime-list))
-        (nconc prime-list (list i)))
-      (setq i (+ i 2)))
+  (if (= upbound 2)
+      (return-from primes-leq (list 2)))
+  (if (= upbound 2)
+      (return-from primes-leq (list 2 3)))
+
+  ;; Take advantage of the fact that prime triples dont exist
+  ;; TODO Generalize to arbitrary step sizes using FLT for coprimality
+  (let* ((prime-list (list 2 3)) (i 6))
+    (loop while (<= (- i 1) upbound) do
+      (when (not (divisor-in-list (- i 1) prime-list))
+        (nconc prime-list (list (- i 1))))
+      (when (not (divisor-in-list (+ i 1) prime-list))
+        (nconc prime-list (list (+ i 1))))
+      (setq i (+ i 6)))
     (return-from primes-leq prime-list)
     )
   )
@@ -24,11 +34,17 @@
 (defun first-n-primes (n)
   (if (< n 1)
       (return-from first-n-primes nil))
-  (let* ((prime-list (list 2)) (i 3))
+
+  ;; See note in primes-leq
+  (let* ((prime-list (list 2 3)) (i 6))
     (loop while (< (length prime-list) n) do
-      (when (not (divisor-in-list i prime-list))
-        (nconc prime-list (list i)))
-      (setq i (+ i 2)))
+      (when (not (divisor-in-list (- i 1) prime-list))
+        (nconc prime-list (list (- i 1))))
+      (when (not (divisor-in-list (+ i 1) prime-list))
+        (nconc prime-list (list (+ i 1))))
+      (setq i (+ i 6)))
+    (when (= (length prime-list) (+ n 1))
+        (return-from first-n-primes (butlast prime-list)))
     (return-from first-n-primes prime-list)
     )
   )
