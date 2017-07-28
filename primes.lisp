9cb868bf62707f75a62f0fa76e9598447d046bb4
Anish Moorthy <anishmoorthy@idevicesinc.com>
2017-07-28 10:22:51 -0400

    Initial commit of (broken) factor method

--- primes.lisp
+++ primes.lisp
@@ -24,6 +24,53 @@
   (return-from divisor-in-slist nil)
   )
 
+(defun factor (n &optional (prime-list nil))
+  "Given a natural number, return (as a list) its prime factorization. If
+   prime-list is non-nil, it must be a sorted list containing at least all
+   primes less than or equal to i"
+  (if (< n 2)
+      (return-from factor nil))
+  (if (= n 2)
+      (return-from factor (list 2)))
+  (if (= n 3)
+      (return-from factor (list 3)))
+
+  ;; TODO This is mostly-duplicated from primes-leq, maybe refactor primes-leq
+  ;; with generators or something to stop that
+  (let* ((i 6) (factor-list nil) (biggest (car (last prime-list))))
+    (dolist (q '(2 3))
+      (loop while (zerop (mod n q)) do
+        (nconc factor-list (list q))
+        (setq n (/ n q))
+            )
+      )
+    (setq i
+          (if (or (not prime-list) (member biggest '(2 3)))
+              6
+              (if (= (mod biggest 6) 1) (+ biggest 5) (+ biggest 1))
+              )
+          )
+    (unless prime-list (setq prime-list (list 2 3)))
+    (loop while (/= n 1) do
+      (unless (divisor-in-slist (- i 1) prime-list)
+        (nconc prime-list (list (- i 1)))
+        (loop while (zerop (mod n (- i 1))) do
+          (nconc factor-list (list (- i 1)))
+          (setq n (/ n (- i 1)))
+          )
+        )
+      (unless (divisor-in-slist (+ i 1) prime-list)
+        (nconc prime-list (list (+ i 1)))
+        (loop while (zerop (mod n (+ i 1))) do
+          (nconc factor-list (list (+ i 1)))
+          (setq n (/ n (+ i 1)))
+          )
+        )
+      (incf i 6))
+    (return-from factor factor-list)
+    )
+  )
+
 (defun primes-leq (upbound &optional (prime-list nil))
   "Return a list of all prime numbers less than or equal to upbound.
   If prime-list is non-nil, it must be a sorted list containing exactly the
