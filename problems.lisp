0e0518aac689e37b44af22d821eac0fefc175466
anish <anish.moorthy@gmail.com>
2017-09-30 15:41:36 -0400

    Create some functions needed for problem 3

--- problems.lisp
+++ problems.lisp
@@ -5,3 +5,18 @@
 (defun problem2 ()
   (loop for x in (fibonacci-leq 4000000) if (evenp x) sum x)
   )
+
+(defun digit (num exp &optional (base 10))
+  "Returns the coefficient of base^exp in base-base representation of num"
+  (mod (floor (/ num (expt base exp))) base))
+
+(defun numdigits (n &optional (base 10))
+  "Returns the number of digits in the base-representation of n"
+  (ceiling (log n base)))
+
+(defun ispalindrome (n &optional (base 10) &aux (len (numdigits n base)))
+  "Returns true if n is palindromic in base, false elsewhere"
+  (loop for x from 0 to (ceiling (/ len 2)) do
+    (if (not (= (digit n x base) (digit n (- len 1 x) base)))
+        (return-from ispalindrome nil)))
+  t)
