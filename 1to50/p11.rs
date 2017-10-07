d0e1f65fb36a7d98538147b9bd43cc828adca6b7
anish <anish.moorthy@gmail.com>
2017-10-07 14:24:34 -0400

    Comments

--- 1to50/p11.rs
+++ 1to50/p11.rs
@@ -1,3 +1,6 @@
+// Rolling products would be much more efficient but the grid is small enough
+// that I can get away without it, so...
+
 fn main() {
     let numstring: &str = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
