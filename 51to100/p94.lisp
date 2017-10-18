6c38742ff0da2e096751596bd9a4f0c4361c547b
anish <anish.moorthy@gmail.com>
2017-10-18 00:31:27 -0400

    This solution really should be working

--- 51to100/p94.lisp
+++ 51to100/p94.lisp
@@ -1,13 +1,20 @@
 (defun solve94 (&aux (max-perimeter 1000000000) (perimeter-sum 0))
-  (loop for ss from 2 upto (/ max-perimeter 3) do
+  (loop for ss from 2 to (/ max-perimeter 3) do
     (loop for su in (list (- ss 1) (+ ss 1)) do
-      (when (and (zerop (mod (triangle-area (list ss ss su)) 1)) (<= (+ ss ss su) max-perimeter))
+      (when (is-perfect-square (triangle-area-squared (list ss ss su)))
+        (format t "~a~%" (list ss ss su))
         (incf perimeter-sum (+ ss ss su)))))
   perimeter-sum)
 
-(defun triangle-area (side-list &aux sp)
+(defun triangle-area-squared (side-list &aux sp)
   "Calculate the area of triangle using Heron's Formula"
   (setq sp (/ (apply '+ side-list) 2))
   (loop for side in side-list with product = 1 do
     (setf product (* product (- sp side)))
-        finally (return-from triangle-area (sqrt (* product sp)))))
+        finally (return-from triangle-area-squared (* product sp))))
+
+(defun is-perfect-square (n)
+  (return-from is-perfect-square (= n (expt (floor (sqrt n)) 2))))
+
+(defun triangle-area (side-list)
+  (sqrt (triangle-area-squared side-list)))
