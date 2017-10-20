e7fc32368028d569f02bb66c40b03280745845da
anish <anish.moorthy@gmail.com>
2017-10-20 01:05:51 -0400

    Commit a not-solution to the problem

---
+++ 51to100/p76.lisp
@@ -0,0 +1,6 @@
+(defun solve76 ()
+  ;; TODO Actually implement a solution for this. The number of paritions
+  ;; of 100 was literally given to us in class as a throwaway example, I just
+  ;; subtracted 1 from it (take out the parition consisting of a single number,
+  ;; the number itself)
+  (- 190569292 1))
